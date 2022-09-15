9ed3c68aa280fb1f0c3788b3c4ff5440b25bb305
ibrahim9999 <mgdlhb@gmail.com>
2022-09-15 15:26:57 +0300

    add readerAllUsersFromData function and ConnDB

--- db_con.php
+++ db_con.php
@@ -1,14 +1,45 @@
 <?php
+function ConnDB()
+{
 $sname="localhost";
 $uname="root";
 $password="";
 $db_name="users";
 $con=mysqli_connect($sname,$uname,$password,$db_name);
 if(!$con)
+    return "ERROR Connection!!!";
+
+return "Connection";
+}
+function readerAllUsersFromdata()//read All into data structure (user_name and password)
 {
-     " ERROR Connection!!! ";
+    $i=0;
+    $USERS=[[]];
+    $sname="localhost";
+    $uname="root";
+    $password="";
+    $db_name="users";
+    $conn=mysqli_connect($sname,$uname,$password,$db_name);
+     if (!$conn) {
+           die("Connection failed: " . mysqli_connect_error());
+       }
+       
+       $sql = "SELECT * FROM users.all_user";
+       $result = mysqli_query($conn, $sql);
+       if (mysqli_num_rows($result) > 0) {
+          
+           while($row = mysqli_fetch_assoc($result)) {
+               //echo " <br> id: " .$row['id']. "<br>  name: ".$row['name'] ."<br> user_name: ".$row['user_name']."<br> password: ".$row['password'];
+               $USERS[$i][1]=$row['password'];
+               $USERS[$i][0]=$row['user_name'];
+               $i++;
+           }
+       } else {
+           echo "0 results";
+       }
+       mysqli_close($conn);
+       return $USERS;
 }
-else{
-    echo "Connection";
-}
+$isCon=readerAllUsersFromdata();
+$Users=ConnDB();
 
--- loginUser.php
+++ loginUser.php
@@ -15,11 +15,34 @@ if(empty($uname) || empty($pass))
         echo"At least one of your details is wrong";
         exit();
     }
-    else{
-         header("Location: open.php");
-       // require_once('db_con.php');
-      //  $sql="SELECT * FROM users where user_name='$uname' and password='$pass'";
-        // $res=mysqli_query($con,$sql);
+else{
+
+       require('db_con.php');
+       /*
+       $conn = mysqli_connect($sname, $uname, $password, $db_name);
+
+       if (!$conn) {
+           die("Connection failed: " . mysqli_connect_error());
+       }
+       
+       $sql = "SELECT * FROM users.all_user";
+       $result = mysqli_query($conn, $sql);
+       if (mysqli_num_rows($result) > 0) {
+          
+           while($row = mysqli_fetch_assoc($result)) {
+               //echo " <br> id: " .$row['id']. "<br>  name: ".$row['name'] ."<br> user_name: ".$row['user_name']."<br> password: ".$row['password'];
+               $password=$row['password'];
+               $user_name=$row['user_name'];
+           }
+       } else {
+           echo "0 results";
+       }
+       
+       
+       mysqli_close($conn);
+       */
+       // header("Location: open.php");
         exit();
     }
 }
+?>
