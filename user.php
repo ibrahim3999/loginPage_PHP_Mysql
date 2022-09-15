632703c751ede1c42abbedb0e85805fb328c0699
ibrahim9999 <mgdlhb@gmail.com>
2022-09-15 15:47:48 +0300

    new class user

---
+++ user.php
@@ -0,0 +1,31 @@
+<?php
+class user{
+    public $id;
+    public $name ;
+    public $user_name;
+    private $password;
+    function __construct($id,$name,$user_name,$password)
+    {
+       $this->id= $id;
+       $this->name= $name;
+       $this->user_name= $user_name;
+       $this->password= $password;
+    }
+    function get_Pass()
+    {
+        return $this->password;
+    }
+    function set_name($name)
+    {
+        $this->name=$name;
+    }
+    function user_name($user_name)
+    {
+        $this->user_name=$user_name;
+    }
+    function password($password)
+    {
+        $this->password=$password;
+    }
+}
+?>
